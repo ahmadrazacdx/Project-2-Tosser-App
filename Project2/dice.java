78f0a98d00a4655ce7b90486f86398cfec11bd88
Ahmad-Raza <123093533+ahmadrazacdx@users.noreply.github.com>
2023-01-21 16:06:54 +0500

    Add files via upload

---
+++ Project2/dice.java
@@ -0,0 +1,11 @@
+package Project2;
+import javax.swing.*;
+public class dice {
+    public static void main(String[] args) {
+        int dice;
+        dice dc=new dice();
+        dice = (int) (Math.random() * 2);
+        if(dice==0) System.out.println("Head");
+        if(dice==1)System.out.println("Tail");
+    }
+}
